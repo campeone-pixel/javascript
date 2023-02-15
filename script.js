701ebb4ab053515ddb3b23f2f19024752a451bdd
campeone-pixel <posesmatiasabel@gmail.com>
2023-02-15 19:21:41 -0300

    simulador terminado

--- script.js
+++ script.js
@@ -1,60 +1,53 @@
-products_dict = {
-	detergente: { stock: 20, price: 200 },
-	aceite: { stock: 1, price: 200 },
-	azucar: { stock: 10, price: 200 },
-	harina: { stock: 10, price: 200 },
-	cerveza: { stock: 10, price: 200 },
-};
-
-function imprimirLista(){
-  console.log("Lista de productos:");
-  for (const key of Object.keys(products_dict)) {
-    console.log(key);
-  }
+class Producto {
+	constructor(nombre, categoria, precio) {
+		this.nombre = nombre;
+		this.precio = precio;
+		this.categoria = categoria;
+	}
 }
 
-function esMayor(edad){
-  if(edad>=18){
-    return true
-  } else{
-    return false
-  }
+let productos = [];
+function agregarProducto(nombre, categoria, precio) {
+	productos.push(new Producto(nombre, categoria, precio));
 }
 
-function existeProduct(lista,product){
-  for (const key of Object.keys(lista)) {
-    if(key === product){
-      return true
-    }
-  }
-  return false
-}
+agregarProducto("arroz", "almacen", 200);
+agregarProducto("aceite", "almacen", 100);
+agregarProducto("vinagre", "almacen", 400);
+agregarProducto("fideos", "almacen", 200);
+agregarProducto("harina", "almacen", 500);
+agregarProducto("detergente", "limpieza", 200);
+agregarProducto("lavandina", "limpieza", 700);
+agregarProducto("esponja", "limpieza", 200);
 
-imprimirLista()
+function eShop() {
+	carrito = [];
 
-eleccion = prompt(
-	"Escriba el producto seleccionado escriba salir para terminar de comprar:"
-);
-
-let total = 0;
-
-while (eleccion != "salir") {
-  
-	if (existeProduct(products_dict,eleccion) & products_dict[eleccion].stock > 0) {
-    total = total + products_dict[eleccion].price
-		products_dict[eleccion].stock = products_dict[eleccion].stock -1
-	} else if (
-		existeProduct(products_dict,eleccion) &
-		products_dict[eleccion].stock <= 0
-	) {
-		console.log("no hay stock")
-	} else {
-		console.log("no existe el producto")
+	for (x of productos) {
+		console.log(x.nombre);
 	}
 
-  eleccion = prompt(
-    "Escriba el producto seleccionado escriba salir para terminar de comprar:"
-  );
+	let eleccion_usuario;
+
+	while (eleccion_usuario != "salir") {
+		eleccion_usuario = prompt("Ingresar nombre producto o escribe salir");
+		let producto = productos.find(
+			(elemento) => elemento.nombre === eleccion_usuario
+		);
+		if (producto != undefined) {
+			carrito.push(producto);
+			console.log("producto agregado al carrito");
+		} else {
+			console.log("el producto no existe");
+		}
+	}
+
+	total = carrito.reduce((acumulador, elemento) => {
+		return elemento.precio + acumulador;
+	}, 0);
+
+	console.log("a terminado su compra");
+	console.log("el total de su compra es " + total);
 }
 
-console.log(`su total es de ${total}`);
+eShop();
